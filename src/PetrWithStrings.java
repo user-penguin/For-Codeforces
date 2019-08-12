0f30051501a6f7a07cd7d528ff4bcb694b843f3d
user-penguin <kobzev96@gmail.com>
2019-08-12 21:32:58 +0700

    completed petya

--- src/PetrWithStrings.java
+++ src/PetrWithStrings.java
@@ -1,2 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PetrWithStrings {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
+        String line2 = br.readLine();
+        String lower1 = line1.toLowerCase();
+        String lower2 = line2.toLowerCase();
+        int cmpr = lower1.compareTo(lower2);
+        if (cmpr != 0) {
+            cmpr = (int)(cmpr/Math.abs(cmpr));
+        }
+        System.out.println(cmpr);
+    }
 }
